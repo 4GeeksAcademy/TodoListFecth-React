b81009d3d445517c88afc3d234e0d89e99d5ef5c
Omar Gutierrez Sanchez <117556558+spirosis@users.noreply.github.com>
2024-01-01 19:36:37 +0000

    esto funciona al pedo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,138 +3,133 @@ import React, { useState, useEffect } from "react";
 const Home = () => {
   const [valueInput, setValueInput] = useState('');
   const [toBeDone, setToBeDone] = useState([]);
+  const [taskCont, setTaskCont] = useState(0);
+  let localList = [...toBeDone]
+
 
   const API_URL = "https://playground.4geeks.com/apis/fake/todos/user/spirosis";
 
-  let getMethod = {
-    method: "GET",
-    headers: {
-      "Content-Type" : "application/json"
+  
+  useEffect(() =>{
+    getTasks()
+    setTaskCont(localList.length)
+  })
+
+  const addTask = async (e) => {
+    try {
+      if (e.key === 'Enter' && valueInput.length != 0) {
+
+          const data = {
+              label: valueInput,
+              done: false
+          }
+
+          localList.push(data)
+
+          await fetch(API_URL, {
+              method: 'PUT',
+              headers: {
+                  'Content-type': 'application/json'
+              },
+              body: JSON.stringify(localList)
+          });
+          setValueInput("")
+
+      }
+  } catch (e) {
+      console.log("addTask function ERROR === ", e)
+  }
+  };
+
+  const getTasks = async () => {
+    try{
+      
+      await fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setToBeDone(data)
+      )
+    }
+    catch (e) {
+      console.log("getTasks error:", e)
+      createUser()
+      location.reload(true)
     }
   }
-  useEffect(() => {
-    // Load tasks from the API when the component mounts
-    fetch(API_URL, getMethod)
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          console.log('request went through');
-          return response.json();
-        } else {
-          console.log(`Error ${response.status} in request`);
-          throw new Error("Failed to fetch data");
-        }
-      })
-      .then(data => {
-        setToBeDone(data);
-        console.log(data);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []); // Empty dependency array to run once when component mounts
 
-  const addTask = () => {
-    if (valueInput.length === 0) {
-      alert("Please, enter a task");
-    } else {
-      // Create a task object
-      const taskObject = {
-        label: valueInput,
-        done: false,
-      };
-  
-      // Use the spread operator to update state immutably
-      setToBeDone(prevState => [taskObject, ...prevState]);
-  
-      // Reset the input field
-      setValueInput('');
-
-      //set method and body to use in fetch
-      let putMethod = {
-        method: "PUT",
-        body: JSON.stringify(toBeDone),
+  const createUser = async () => {
+    try {
+      await fetch(API_URL, {
+        method: 'POST',
         headers: {
-          "Content-Type" : "application/json"
-        }
-      }
-
-      // Save tasks to the API using an empty array in the request body
-      fetch(API_URL, putMethod)
-        .then(response => {
-          if (response.status >= 200 && response.status < 300) {
-            console.log('Data saved successfully');
-            return response.json();
-          } else {
-            console.log(`Error ${response.status} in request`);
-            return response.text().then(errorText => {
-              console.error('API error:', errorText);
-              throw new Error("Failed to save data");
-            });
-          }
-        })
-        .catch(error => console.error('Error saving data:', error));
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify([])
+      });
     }
-  };
-  
+    catch (e) {
+      console.log("createUser function ERROR ===", e)
+    }
+  }
 
-  const deleteTask = (taskIndex) => {
-    // Use the spread operator to update state immutably
-    setToBeDone(prevState => prevState.filter((_, i) => i !== taskIndex));
+  const deleteTask = async (ele) => {
+    try {
+      const newTasks = toBeDone.filter((task) =>{ return ele !== task.id});
 
-    let deleteMethod = {
+      await fetch(API_URL, {
         method: "PUT",
-        body: JSON.stringify(toBeDone),
         headers: {
           "Content-Type": "application/json",
         },
-      }
-
-    
-    // Save tasks to the API
-    fetch(API_URL, deleteMethod)
-      .then(response => {
-        if (response.status >= 200 && response.status < 300) {
-          console.log('Data saved successfully');
-          return response.json();
-        } else {
-          console.log(`Error ${response.status} in request`);
-          throw new Error("Failed to save data");
-        }
-      })
-      .catch(error => console.error('Error saving data:', error));
+        body: JSON.stringify(newTasks),
+      });
+      setValueInput("");
+      console.log('deletion completed')
+    }
+    catch(e) {
+      console.error('Error deleting data:', e);
+    }
+      
   };
 
-  
+  const deleteAllTasks = async () =>{
+    try{
+      await fetch(API_URL, 
+        {method: 'DELETE'})
+    }catch (e){
+      console.log('deleteAllTasks function ERROR', e)
+    }
+  };
 
   return (
-    <div className='list'>
-      <h1>Task Manager Quantico</h1>
-      <ul>
-        <li>
-          <input
-            type="text"
-            value={valueInput}
-            onChange={(e) => setValueInput(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.code === 'Enter') {
-                addTask();
-              }
-            }}
-          />
-          <button onClick={addTask}>+</button>
-        </li>
-        {toBeDone.map((task, index) => (
-          <li key={index}>
-            {task.label}
-            <button onClick={() => deleteTask(index)}>
-              <i className="far fa-trash-all">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-                  <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                </svg>
-              </i>
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div>{toBeDone.length} Items left</div>
+    <div className='list container-fluid'>
+      <div>
+        <h1>Task Manager Quantico</h1>
+          <ul>
+            <li>
+              <input
+                type="text"
+                placeholder="whats for today?"
+                value={valueInput}
+                onChange={(e) => setValueInput(e.target.value)}
+                onKeyDown= {addTask}
+              />
+              <button onClick={addTask}>+</button>
+            </li>
+            <div>
+
+            {toBeDone.map((task, index) => (
+              <li key={index}>
+                {task.label}
+                <button onClick={() => deleteTask(task.id)}>
+                &#x2715;
+                </button>
+              </li>
+            ))}
+
+            </div>
+          </ul>
+        <div className="taskCont">{taskCont} Items left</div>
+      </div>
     </div>
   );
 };
